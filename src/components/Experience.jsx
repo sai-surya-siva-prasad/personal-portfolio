f9ffc478e18308a255e1c2eb359e2ad5eb55d0aa
Sai Surya Siva Prasad Raghavarapu <49845425+sai-surya-siva-prasad@users.noreply.github.com>
2023-06-19 02:37:05 -0400

    Update Experience.jsx
    experience moved down

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import html from "../assets/html.png";
 import css from "../assets/css.png";
 import javascript from "../assets/javascript.png";
@@ -74,13 +73,13 @@ const Experience = () => {
   ];
 
   return (
-    <div name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full h-screen">
+    <div name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full min-h-screen">
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
-        <div>
+        <div className="mb-8">
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
-          <p className="py-6">These are the technologies I have worked with</p>
+          <p className="pt-6">These are the technologies I have worked with</p>
         </div>
-        <div className="py-4">
+        <div className="pb-4">
           <button
             className={`bg-green-500 text-white px-4 py-2 rounded ${isRotating ? "opacity-100" : "opacity-50"}`}
             onClick={toggleRotation}
