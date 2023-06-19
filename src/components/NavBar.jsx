9f30145c1d206313ef05381c8abd9db149fad4fe
Sai Surya Siva Prasad Raghavarapu <49845425+sai-surya-siva-prasad@users.noreply.github.com>
2023-06-19 19:35:06 -0400

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed z-50">
       <div>
-        <h1 className="text-5xl font-signature ml-2 text-white">Siya</h1>
+        <h1 className="text-5xl font-signature ml-2 text-white">Surya</h1>
       </div>
 
       <ul className="hidden md:flex">
