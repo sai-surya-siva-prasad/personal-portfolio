2eb32a4358f055b4d711dcd144cc9902089a5f7d
Sai Surya Siva Prasad Raghavarapu <49845425+sai-surya-siva-prasad@users.noreply.github.com>
2023-06-22 17:19:49 -0400

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -43,7 +43,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: '/resume Apex.pdf',
+      href: '/Resume June22g.pdf',
       style: 'rounded-br-md',
       download: true,
     },
